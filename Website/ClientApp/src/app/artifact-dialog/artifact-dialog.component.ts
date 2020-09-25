6b9faf7364df60de9f530c0df0dbaece8bae1c3c
Da-Teach <da_teach@thehackerwithin.com>
2020-09-25 16:31:14 +0200

    Added filter/sort to artifact dialog

--- Website/ClientApp/src/app/artifact-dialog/artifact-dialog.component.ts
+++ Website/ClientApp/src/app/artifact-dialog/artifact-dialog.component.ts
@@ -3,6 +3,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Hero } from '../shared/clients';
 import { Artifact } from '../shared/clients';
 import { RaidAccount } from '../shared/raid-account';
+import { Raid } from '../shared/raid';
 
 @Component({
   selector: 'app-artifact-dialog',
@@ -11,6 +12,10 @@ import { RaidAccount } from '../shared/raid-account';
 export class ArtifactDialogComponent {
   artifacts: Artifact[];
   account: RaidAccount;
+  equippedArtifacts: number[] = [];
+
+  filter = 'all';
+  orderBy = 'default';
 
   constructor(
     private dialogRef: MatDialogRef<ArtifactDialogComponent>,
@@ -21,7 +26,35 @@ export class ArtifactDialogComponent {
     }
   ) {
     this.account = data.account;
-    this.artifacts = data.artifacts.sort((a, b) => {
+    for (let h of this.account.heroes) {
+      if (!h.artifacts || h.id === data.hero.id) {
+        continue;
+      }
+
+      for (let id of h.artifacts) {
+        this.equippedArtifacts.push(id);
+      }
+    }
+
+    this.refreshArtifacts();
+  }
+
+  refreshArtifacts() {
+    let baseValue = 100;
+    if (this.orderBy !== 'default' && !this.orderBy.startsWith('Crit')) {
+      baseValue = this.data.hero[Raid.statProperty[this.orderBy]];
+    }
+
+    this.artifacts = this.data.artifacts.sort((a, b) => {
+      if (this.orderBy !== 'default') {
+        const statA = this.account.getBonusValue(a, baseValue, this.orderBy);
+        const statB = this.account.getBonusValue(b, baseValue, this.orderBy);
+        const stat = statB - statA;
+        if (stat !== 0) {
+          return stat;
+        }
+      }
+
       let c = a.setKind.localeCompare(b.setKind);
       if (c !== 0) return c;
       c = a.primaryBonus.kind.localeCompare(b.primaryBonus.kind);
@@ -35,9 +68,11 @@ export class ArtifactDialogComponent {
       c = a.rarity.localeCompare(b.rarity);
       return c;
     }).filter(a => {
+      if (this.filter === 'not-equipped' && this.equippedArtifacts.indexOf(a.id) >= 0) return false;
       if (!a.requiredFraction) return true;
-      return a.requiredFraction === data.hero.fraction;
+      return a.requiredFraction === this.data.hero.fraction;
     });
+
   }
 
   select(artifact: Artifact) {
