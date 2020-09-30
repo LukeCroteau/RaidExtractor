d088f3077af7eb5a7f673bb8dc645c6ce9eff2d5
Da-Teach <da_teach@thehackerwithin.com>
2020-09-30 09:34:04 +0200

    Changed importance slider from 0 to 3 to 0 to 16 to allow better stat weights

--- Website/ClientApp/src/app/optimize-dialog/optimize-dialog.component.ts
+++ Website/ClientApp/src/app/optimize-dialog/optimize-dialog.component.ts
@@ -254,15 +254,14 @@ export class OptimizeDialogComponent {
       this.weightHealth, this.weightAttack, this.weightDefense, this.weightSpeed, this.weightCriticalChance,
       this.weightCriticalDamage, this.weightResistance, this.weightAccuracy
     ];
-    const weightFactor = [0, 1, 4, 8, 16];
 
     let totalWeight = 0;
     const result: StatValue[] = [];
     for (let i = 0; i < maxValues.length; i ++) {
       if (maxValues[i] === 0) continue;
-      const weight = weightFactor[weights[i]] / maxValues[i];
+      const weight = weights[i] / maxValues[i];
       if (weight === 0) continue;
-      totalWeight += weightFactor[weights[i]];
+      totalWeight += weights[i];
       result.push(new StatValue(['Health','Attack','Defense','Speed','CriticalChance','CriticalDamage','Resistance','Accuracy'][i], weight));
     }
 
@@ -275,6 +274,10 @@ export class OptimizeDialogComponent {
   }
 
   weightDisplay(value: number): string {
-    return ['-', '+', '++', '+++'][value];
+    if (value) {
+      return `+${value}`;
+    } else {
+      return 'NI';
+    }
   }
 }
