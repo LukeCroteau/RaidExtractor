eb480b0aa63ea8c8df0352f698a4e0f23ebf71cc
Luke Croteau <luke.j.croteau@gmail.com>
2021-01-08 00:34:20 -0500

    Fix Website support, allow push to Azure services

--- RaidExtractor/AppSettings.Designer.cs
+++ RaidExtractor/AppSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace RaidExtractor {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class AppSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static AppSettings defaultInstance = ((AppSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new AppSettings())));
@@ -25,7 +25,7 @@ namespace RaidExtractor {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:7418")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:5000")]
         public string BaseUrl {
             get {
                 return ((string)(this["BaseUrl"]));
