dfee5107dbc0e86968286df08fd493dcfda7c7d4
Luke Croteau <luke.j.croteau@gmail.com>
2021-01-10 15:01:30 -0500

    Restore Dev Version Info. Ignore all future Version Info changes

--- Website/ClientApp/src/app/shared/version-info.ts
+++ Website/ClientApp/src/app/shared/version-info.ts
@@ -1,25 +1,12 @@
 // ReSharper disable StringLiteralWrongQuotes
 export const versionInfo = {
-  "dirty": true,
-  "raw": "v0.8.14-2-g8b54c55-dirty",
-  "hash": "g8b54c55",
-  "distance": 2,
-  "tag": "v0.8.14",
-  "semver": {
-    "options": {
-      "loose": false,
-      "includePrerelease": false
-    },
-    "loose": false,
-    "raw": "v0.8.14",
-    "major": 0,
-    "minor": 8,
-    "patch": 14,
-    "prerelease": [],
-    "build": [],
-    "version": "0.8.14"
-  },
-  "suffix": "2-g8b54c55-dirty",
-  "semverString": "0.8.14+2.g8b54c55.dirty"
+  "dirty": false,
+  "raw": "",
+  "hash": "",
+  "distance": 0,
+  "tag": "v0.0-dev",
+  "semver": null,
+  "suffix": "",
+  "semverString": ""
 };
 // ReSharper restore StringLiteralWrongQuotes
