72988fa114e3a89d79733ae583b269c56144d90e
Luke Croteau <luke.j.croteau@gmail.com>
2021-01-17 01:50:20 -0500

    Removed Console Application. Added Command Line options

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # RaidExtractor
-A tool made to extract information from the windows version of "Raid: Shadow Legends". Currently it supports v0.229 (as seen in Plarium Play launcher) and currently only extracts artifacts.
+A tool made to extract information from the windows version of "Raid: Shadow Legends". Currently it supports v0.230 (as seen in Plarium Play launcher) and currently only extracts artifacts and current champions.
 
-This initial version is just a preview to see if people are interested in it before continueing development. Now in a "Fancy" windows application (instead of a console application).
+This application has 2 Modes:
+* A Windows GUI application. The functionality is slim, but it works!
+* A Non-GUI mode, which simply outputs the files as required.
+  * Include **--nogui** or **-n** when running the application to run without a GUI.
+  * Use **-o "output file name and path"** to specify the output file.
+  * Use **-t "zip/json"** to pick the output mode. By default, this is set to **json**
+  * If No Parameters are specified other than **--nogui/-n**, the application will create an *artifacts.json* file right where the application is run.
 
 ## Requirements
 * .NET Framework 4.8
 
 ## Future versions
-* Add Great Hall data
+* Add Great Hall and Arena data
 
 ## Feedback
-Use Issues if you would like to provide feedback or request features. 
-
-## Known Issues
-* Health/Attack/Defense calculations may be off by a few points
+Please use the Github Issues page for this project, located at https://github.com/LukeCroteau/RaidExtractor/issues for any currently known issues, or to report any bugs!
--- RaidExtractor/Program.cs
+++ RaidExtractor/Program.cs
@@ -1,16 +1,101 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.IO.Compression;
+using CommandLine;
+using RaidExtractor.Core;
+using Newtonsoft.Json;
 
 namespace RaidExtractor
 {
     class Program
     {
-        [STAThread]
-        static void Main(string[] args)
+        public class Options
+        {
+            [Option('g', "nogui", Required = false, Default = false,
+              HelpText = "Run this program without a GUI.")]
+            public bool NoGui { get; set; }
+
+            [Option('o', "output", Required = false, Default = "artifacts.json",
+              HelpText = "Destination output file name.\nDefaults to artifacts.json")]
+            public string OutputFile { get; set; }
+
+            [Option('t', "type", Required = false, Default = "json", HelpText = "Output Type: 'json' for JSON file output, 'zip' for ZIP file output.")]
+            public string DumpType { get; set; }
+        }
+
+        static void RunGUI()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                RunGUI();
+            }
+            else
+            {
+                var options = new Options();
+
+                CommandLine.Parser.Default.ParseArguments<Options>(args)
+                    .WithParsed<Options>(o =>
+                    {
+                        if (!o.NoGui)
+                        {
+                            RunGUI();
+                            return;
+                        }
+
+                        Extractor raidExtractor = new Extractor();
+                        AccountDump dump;
+                        try
+                        {
+                            dump = raidExtractor.GetDump();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"There was an error during Extraction: {ex.Message}");
+                            return;
+                        }
+
+                        var outFile = o.OutputFile;
+
+                        var json = JsonConvert.SerializeObject(dump, Formatting.Indented);
+                        if (o.DumpType.ToLower() == "zip")
+                        {
+                            if (!outFile.ToLower().Contains("zip")) outFile += ".zip";
+                            File.Delete(outFile);
+
+                            using (var memoryStream = new MemoryStream())
+                            {
+                                using (ZipArchive archive = ZipFile.Open(outFile, ZipArchiveMode.Create))
+                                {
+                                    var artifactFile = archive.CreateEntry("artifacts.json");
+
+                                    using (var entryStream = artifactFile.Open())
+                                    {
+                                        using (var streamWriter = new StreamWriter(entryStream))
+                                        {
+                                            streamWriter.Write(json);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (o.DumpType.ToLower() != "json") Console.WriteLine("Unknown Output type. Outputting file in JSON format.");
+                            File.WriteAllText(outFile, json);
+                        }
+                        Console.WriteLine($"Output file {outFile} has been created.");
+                    });
+            }
         }
     }
 }
