34a28fe8ff9d809f687311f7754d8e0d0fa543f5
Luke Croteau <luke.j.croteau@gmail.com>
2021-03-16 09:58:18 -0400

    Update addresses for Raid v234

--- RaidExtractor.Core/Native/RaidStaticInformation.cs
+++ RaidExtractor.Core/Native/RaidStaticInformation.cs
@@ -2,24 +2,24 @@ namespace RaidExtractor.Core.Native
 {
     class RaidStaticInformation
     {
-        public static string ExpectedRaidVersion = "\\233\\";
-        public static int MemoryLocation = 55440936;
-        public static int ExternalStorageAddress = 55624744;
+        public static string ExpectedRaidVersion = "\\234\\";
+        public static int MemoryLocation = 55952624;
+        public static int ExternalStorageAddress = 56120680;
 
         public static int UserHeroDataHeroById = 0x18; // UserHeroData.HeroById
 
         public static int UserArtifactDataArtifacts = 0x28; // UserArtifactData.Artifactsa
         public static int UserArtifactArtifactDataByHeroId = 0x30; // UserArtifactData.ArtifactDataByHeroId
 
-        public static int AppModelUserWrapper = 0x150; // AppModel._userWrapper
+        public static int AppModelUserWrapper = 0x148; // AppModel._userWrapper
 
         public static int UserWrapperHeroes = 0x28; // UserWrapper.Heroes
         public static int UserWrapperShards = 0x70; // UserWrapper.Shards
         public static int UserWrapperArena = 0xB0; // UserWrapper.Arena
         public static int UserWrapperCapitol = 0xC8; // UserWrapper.Capitol
 
-        public static int HeroesWrapperArtifactData = 0x58; // HeroesWrapperReadOnly.ArtifactData
-        public static int HeroesWrapperHeroData = 0x48; // HeroesWrapperReadOnly.HeroData
+        public static int HeroesWrapperArtifactData = 0x60; // HeroesWrapperReadOnly.ArtifactData
+        public static int HeroesWrapperHeroData = 0x50; // HeroesWrapperReadOnly.HeroData
 
         public static int DictionaryCount = 0x20; // Dictionary.Count
     }
